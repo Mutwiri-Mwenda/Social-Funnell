cdf199cdf01d09879b3741a8f113cd1dc227bd81
Mutwiri <65445617+Mutwiri-Mwenda@users.noreply.github.com>
2023-07-12 20:29:07 +0300

    Add files via upload

---
+++ CTA.jsx
@@ -0,0 +1,19 @@
+import styles from '../style';
+import Button from './Button';
+const CTA = () =>  (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={`${styles.heading2}`}>
+          Let's try our service now!
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Everything you need to accept card payments and grow your business anywhere on the planet.
+        </p>
+      </div>
+      <div>
+        <Button/>
+      </div>
+    </section>
+  )
+
+export default CTA
