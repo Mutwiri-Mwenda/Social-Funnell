6660b730dbb4817975e63c5771714723d7695a0a
Mutwiri <65445617+Mutwiri-Mwenda@users.noreply.github.com>
2023-07-15 22:28:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Deploy from "./Deploy";
+import Audit from "./Audit";
+import Approach from "./Approach";
+
+
+
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  Deploy,
+  Audit,
+  Approach,
+};
